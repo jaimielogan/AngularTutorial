ec5ed7dbcf7f13656d64d69db17f54f359770ea2
jaimielogan <jaimielogan@gmail.com>
2016-10-12 11:02:38 -0600

    Events completed

--- events/app.js
+++ events/app.js
@@ -17,15 +17,52 @@ app.controller('MainController', function($scope){
   // PingPong
   $scope.view.enterCount1 = 0;
   $scope.view.enterCount2 = 0;
+  $scope.view.gameover = false;
+  $scope.view.winner = '';
+  $scope.view.serveCount = 0;
+  $scope.view.server = true;
+  // ** check to see if winner, if not add to score
+  // if winner then set as winner and make gameover true
+  // ** Make a serving increment?
   $scope.addScore1 = function(){
-    $scope.view.enterCount1 += 1;
+    if($scope.view.gameover === false){
+      $scope.view.enterCount1 += 1;
+      $scope.view.serveCount++;
+      if($scope.view.serveCount % 2 !== 0){
+        $scope.view.server = false;
+      }
+      else if ($scope.view.serveCount % 2 === 0) {
+        $scope.view.server = true;
+      }
+      if($scope.view.enterCount1 >= 11){
+        $scope.view.winner = 'player1';
+        $scope.view.gameover = true;
+      }
+    }
   };
   $scope.addScore2 = function(){
-    $scope.view.enterCount2 += 1;
+    if($scope.view.gameover === false){
+      $scope.view.enterCount2 += 1;
+      $scope.view.serveCount++;
+      if($scope.view.serveCount % 2 !== 0){
+        $scope.view.server = false;
+      }
+      else if ($scope.view.serveCount % 2 === 0) {
+        $scope.view.server = true;
+      }
+      if($scope.view.enterCount2 >= 11){
+        $scope.view.winner = 'player2';
+        $scope.view.gameover = true;
+      }
+    }
   };
   $scope.reset = function(){
     $scope.view.enterCount1 = 0;
     $scope.view.enterCount2 = 0;
+    $scope.view.gameover = false;
+    $scope.view.winner = '';
+    $scope.view.serveCount = 0;
+    $scope.view.server = true;
   };
 
 
@@ -41,10 +78,19 @@ app.controller('MainController', function($scope){
     $scope.view.colors.push(color);
     return color;
   };
-  $scope.replay = function(colors){
-    var displayPrevColor = function(){
-      $scope.view.colors = colors.reverse();
-
+  $scope.replay = function() {
+    var displayPrevColor = function() {
+      // do some logic to change color
+      // if done replay colors
+      replaying = false;
+      // else
+      $timeout(displayPrevColor, 1000);
+      // end if/else
     };
+    if (!replaying) {
+      replaying = true;
+      // This timeout starts the timeout loop
+      $timeout(function() { displayPrevColor(); }, 500);
+    }
   };
 });
