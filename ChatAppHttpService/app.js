ccc0ce093e27899c9c57b8d471868fbf095a94ab
jaimielogan <jaimielogan@gmail.com>
2016-10-17 12:01:19 -0600

    Chat App. HTTP Get and Post to messages API

---
+++ ChatAppHttpService/app.js
@@ -0,0 +1,24 @@
+var chatApp = angular.module('chatApp', []);
+
+chatApp.controller('MainController', function($scope, $http){
+  $scope.view = {};
+
+  $http.get('https://messagehttpservice.herokuapp.com/messages')
+  .success(function(data){
+    $scope.view.messages = data;
+  })
+  .catch(function(error){
+    $scope.view.messages = error.status;
+  });
+
+  $scope.submitQuote = function(){
+    var data = {
+      message: {
+        name: $scope.view.name,
+        content: $scope.view.quote
+      }
+    };
+
+    $http.post('https://messagehttpservice.herokuapp.com/messages', data);
+  };
+});
